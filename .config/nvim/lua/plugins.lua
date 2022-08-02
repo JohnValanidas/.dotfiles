e89f64e4edc7f2dbda942b0b545d165a28f61f82
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2022-08-02 14:25:19 -0400

    remove file

--- .config/nvim/lua/plugins.lua
+++
@@ -1,6 +0,0 @@
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
-end)
